1d88df1b57e55f004f37c71611b2bb6a1f4af90a
lacinisayev <91473189+lacinisayev@users.noreply.github.com>
2022-03-29 23:08:58 +0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function pasuser(form){
-    if (form.id.value=="ulvi.m221@mail.ru"){
-    if (form.pass.value=="ulvi2003"){   
-    location="https://www.youtube.com/watch?v=CCx7hyxI6kU"} 
-    else {message=document.getElementById("error-message2")
-          message.innerHTML='<i class="fa-solid fa-triangle-exclamation"></i>Incorrect password';}}
-
-    else {message=document.getElementById("error-message1")
-          message.innerHTML='<i class="fa-solid fa-triangle-exclamation"></i>Enter an email or phone number';}}
